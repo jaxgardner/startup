0e12c9351fec46563e3888fe1970898f83acffe4
Jaxon Gardner <pjaxongardner@gmail.com>
2023-11-02 09:55:18 -0600

    Drafted endpoints

--- webservice.js
+++ webservice.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const app = express();
 
+// Create new user
+app.post('/signup', (req, res, next) => {
+    res.send({username: 'Jaxrocs'});
+})
 
+// Login returning user
+app.post('/login', (req, res) => {})
+
+
+// Get current user profile
+app.get('/user', (req, res) => {})
+
+
+// Get saved activities
+app.get('/saved', (req, res) => {})
+
+
+// Get a quote from webservice
+app.get('/quote', (req, res) => {})
+
+
+// Update user profile info
+app.patch('/user/:userId', (req, res) => {})
+
+
+// Delete saved activity
+app.delete('/saved/:activityId', (req, res) => {})
+
+
+// Delete user account
+app.delete('user/:userId', (req, res) => {})
 
 app.use(express.static('public'));
-
 
 const port = 8080;
 app.listen(port, function () {
